09179520687b36d477f46af41da8529cbba5d9cc
feijie <feijie89@qq.com>
2019-03-03 10:55:34 +0800

    优化配置

--- ImageApi/Startup.cs
+++ ImageApi/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Swashbuckle.AspNetCore.Swagger;
@@ -19,6 +20,17 @@ namespace ImageApi
 {
     public class Startup
     {
+        private const string DefaultCorsPolicyName = "public";
+
+        private readonly IConfigurationRoot _appConfiguration;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _hostingEnvironment = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -35,6 +47,22 @@ namespace ImageApi
                 {
                     builder.UseSqlServer("Server=localhost; Database=ImageDb; Trusted_Connection=True;");
                 });
+            services.AddCors(options =>
+            {
+                options.AddPolicy(DefaultCorsPolicyName, builder =>
+                {
+                    builder
+                        .WithOrigins(
+                            _appConfiguration["CorsOrigins"]
+                                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                                .ToArray()
+                        )
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info {Title = "图片资源API", Version = "v1"});
@@ -49,9 +77,18 @@ namespace ImageApi
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(DefaultCorsPolicyName);
             app.UseMvcWithDefaultRoute();
             app.UseSwagger()
                 .UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "图片资源API"); });
+        }
+    }
+
+    public static class HostingEnvironmentExtensions
+    {
+        public static IConfigurationRoot GetAppConfiguration(this IHostingEnvironment env)
+        {
+            return AppConfigurations.Get(env.ContentRootPath, env.EnvironmentName);
         }
     }
 }
